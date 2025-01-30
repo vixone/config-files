c0c93f5b871c060e7e698c6ed6058ed11487e0a8
victor chiriac <victor.chiriac@gmail.com>
2025-01-30 17:44:29 +0200

    add latest lunarvim config

--- lvim/lvim/config.lua
+++ lvim/lvim/config.lua
@@ -4,12 +4,16 @@ vim.g.neotree_auto_cd = 0
 lvim.builtin.nvimtree.active = false -- NOTE: using neo-tree
 
 lvim.format_on_save.enabled = true
-lvim.format_on_save.pattern = { "*.lua", "*.php" }
+lvim.format_on_save.pattern = { "*.lua", "*.php", "*.js", "*.jsx", "*.ts", "*.tsx", "*.vue" }
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
   {
     name = "pint",
     filetypes = { "php" },
+  },
+  {
+    name = "eslint",
+    filetypes = { "*.vue", "*.js", "*.jsx", "*.ts", "*.tsx" },
   },
 })
 -- KEYBINDINGS --
@@ -55,8 +59,26 @@ lvim.keys.normal_mode["H"] = ":BufferLineCyclePrev<CR>"
 
 lvim.plugins = {
   { "lunarvim/colorschemes" },
+  { "nvim-pack/nvim-spectre" },
   { "catppuccin/nvim" },
   { "sainnhe/gruvbox-material" },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({})
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function()
+      require("copilot_cmp").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false }
+      })
+    end
+  },
   {
     "nvim-neo-tree/neo-tree.nvim",
     config = function()
@@ -97,6 +119,22 @@ lvim.plugins = {
     },
   },
 }
+-- Below config is required to prevent copilot overriding Tab with a suggestion
+-- when you're just trying to indent!
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+end
+local on_tab = vim.schedule_wrap(function(fallback)
+  local cmp = require("cmp")
+  if cmp.visible() and has_words_before() then
+    cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+  else
+    fallback()
+  end
+end)
+lvim.builtin.cmp.mapping["<Tab>"] = on_tab
 
 lvim.keys.normal_mode["<leader>r"] = { ":NeoTreeReveal<cr>", noremap = true, silent = true }
 
